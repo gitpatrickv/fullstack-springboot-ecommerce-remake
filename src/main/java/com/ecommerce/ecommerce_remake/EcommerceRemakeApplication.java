27a5b75c7cbded02e1b853eafb437dcedf500e0c
rektgit2024 <myemailispatrick@gmail.com>
2024-12-25 18:48:14 +0800

    initial commit

---
+++ src/main/java/com/ecommerce/ecommerce_remake/EcommerceRemakeApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce.ecommerce_remake;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceRemakeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceRemakeApplication.class, args);
+	}
+
+}
